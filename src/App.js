d1b63f487affe962d98a4d666659ee0af1d4fcc2
Elif Pektezel <140959618+ElifPektezel@users.noreply.github.com>
2023-12-20 06:12:21 +0300

    Menubar&Header section added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { CssBaseline, ThemeProvider  } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Topbar from './scenes/global/Topbar'
+import Menubar from "./scenes/global/Menubar";
+import Dashboard from "./scenes/dashboard";
+import { Routes, Route } from "react-router-dom";
+//import Team from "./scenes/team";
+//import Invoices from "./scenes/invoices";
+//import Contacts from "./scenes/contacts";
+//import Bar from "./scenes/bar";
+//import Form from "./scenes/form";
+//import Line from "./scenes/line";
+//import Pie from "./scenes/pie";
+//import FAQ from "./scenes/faq";
+//import Geography from "./scenes/geography";
+//import Calendar from "./scenes/calendar/calendar";
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -9,8 +22,12 @@ function App() {
       <ThemeProvider  theme={theme}>
         <CssBaseline/>
         <div className="app">
+          <Menubar/>
           <main className="content">
             <Topbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
