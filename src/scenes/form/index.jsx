b2c8b144c9f89df0ee1e5c9a9d8a56c8555a77e0
Elif Pektezel <140959618+ElifPektezel@users.noreply.github.com>
2023-12-25 08:02:24 +0300

    form&team section edited.Online creat user.

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,20 +1,64 @@
+import React from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import Header from "../../components/Header";
 
-const Form = () => {
+const Form = ({ onCreateUser, onUserCreated }) => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
 
-  const handleFormSubmit = (values) => {
-    console.log(values);
+  const handleFormSubmit = (values, { resetForm }) => {
+    const newUser = {
+      id: Date.now(),
+      ...values,
+    };
+
+    // Yeni kullanıcıyı ekleyen fonksiyonu çağırdık.
+    if (onCreateUser && typeof onCreateUser === "function") {
+      onCreateUser(newUser);
+    }
+
+    // Formu sıfırladık.
+    resetForm();
+
+    // Kullanıcı oluşturulduğunda ana bileşene bildirdik.
+    //onUserCreated fonksiyonun gerçek bir fonksiyon olup olmadıpını kont ettik.
+    if (onUserCreated && typeof onUserCreated === "function") {
+      onUserCreated(newUser);
+    }
+
+    // Kullanıcı bilgilerini localStorage'a ekledik.
+    const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const updatedUsers = [...storedUsers, newUser];
+    localStorage.setItem("users", JSON.stringify(updatedUsers));
+  };
+
+  const phoneRegExp =
+    /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+  const checkoutSchema = yup.object().shape({
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    contact: yup
+      .string()
+      .matches(phoneRegExp, "Phone number is not valid")
+      .required("required"),
+    address1: yup.string().required("required"),
+    address2: yup.string().required("required"),
+  });
+
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    address1: "",
+    address2: "",
   };
 
   return (
     <Box m="20px">
-      <Header title="CREATE USER" subtitle="Create a New User Profile" />
-
       <Formik
         onSubmit={handleFormSubmit}
         initialValues={initialValues}
@@ -126,29 +170,6 @@ const Form = () => {
       </Formik>
     </Box>
   );
-};
-
-const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
-
-const checkoutSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, "Phone number is not valid")
-    .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
-});
-const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  address1: "",
-  address2: "",
 };
 
 export default Form;
--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -1,70 +1,59 @@
+import React, { useState, useEffect } from "react";
 import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
-import { mockDataTeam } from "../../data/mockData";
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
+import { tokens } from "../../theme";
 
 const Team = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    // Sayfa yüklendiğinde localStorage'dan kullanıcı bilgilerini aldık.
+    const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
+    setUserList(storedUsers);
+  }, []);
+
   const columns = [
     { field: "id", headerName: "ID" },
-    {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      cellClassName: "name-column--cell",
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      headerAlign: "left",
-      align: "left",
-    },
-    {
-      field: "phone",
-      headerName: "Phone Number",
-      flex: 1,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
-    },
+    { field: "firstName", headerName: "First Name", flex: 1 },
+    { field: "lastName", headerName: "Last Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "contact", headerName: "Contact Number", flex: 1 },
+    { field: "address1", headerName: "Address 1", flex: 1 },
+    { field: "address2", headerName: "Address 2", flex: 1 },
     {
       field: "accessLevel",
       headerName: "Access Level",
       flex: 1,
-      renderCell: ({ row: { access } }) => {
-        return (
-          <Box
-            width="60%"
-            m="0 auto"
-            p="5px"
-            display="flex"
-            justifyContent="center"
-            backgroundColor={
-              access === "admin"
-                ? colors.greenAccent[600]
-                : access === "manager"
-                ? colors.greenAccent[700]
-                : colors.greenAccent[700]
-            }
-            borderRadius="4px"
-          >
-            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-            {access === "manager" && <SecurityOutlinedIcon />}
-            {access === "user" && <LockOpenOutlinedIcon />}
-            <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
-              {access}
-            </Typography>
-          </Box>
-        );
-      },
+      renderCell: ({ row: { accessLevel } }) => (
+        <Box
+          width="60%"
+          m="0 auto"
+          p="5px"
+          display="flex"
+          justifyContent="center"
+          backgroundColor={
+            accessLevel === "admin"
+              ? colors.greenAccent[600]
+              : accessLevel === "manager"
+              ? colors.greenAccent[700]
+              : colors.greenAccent[700]
+          }
+          borderRadius="4px"
+        >
+          {accessLevel === "admin" && <AdminPanelSettingsOutlinedIcon />}
+          {accessLevel === "manager" && <SecurityOutlinedIcon />}
+          {accessLevel === "user" && <LockOpenOutlinedIcon />}
+          <Typography color={colors.grey[100]} sx={{ ml: "5px" }}>
+            {accessLevel}
+          </Typography>
+        </Box>
+      ),
     },
   ];
 
@@ -100,7 +89,12 @@ const Team = () => {
           },
         }}
       >
-        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+        <DataGrid
+          checkboxSelection
+          rows={userList}
+          columns={columns}
+          disableSelectionOnClick
+        />
       </Box>
     </Box>
   );
